3ec959e5d2480f979d55edf81a312185e614358e
taknife <taknife404@gmail.com>
2025-08-21 15:17:03 +0800

    优化主函数逻辑

--- src/main.py
+++ src/main.py
@@ -6,47 +6,12 @@
 # @File    : main.py
 import sys
 import configuration_clear
-import device_link
-from PySide6 import QtGui
-from PySide6.QtCore import QStandardPaths, QObject, Signal, QThread
-from PySide6.QtGui import QIcon
+from task_modules import *
 from functools import partial
+from PySide6.QtCore import QStandardPaths
+from PySide6.QtGui import QIcon, QIntValidator
 from PySide6.QtWidgets import QApplication, QWidget, QFileDialog, QTextBrowser
 from PySide6.QtUiTools import QUiLoader
-
-# 自定义信号源对象类型，一定要继承自 QObject
-class MySignals(QObject):
-    # 定义一种信号，两个参数 类型分别是： QTextBrowser 和 字符串
-    # 调用 emit方法 发信号时，传入参数 必须是这里指定的 参数类型
-    text_print = Signal(QTextBrowser,str)
-    # 还可以定义其他种类的信号
-    update_table = Signal(str)
-# 实例化
-global_ms = MySignals()
-
-class WorkerThread(QThread):
-    def __init__(self, ip, port, username, password, config_module, parent=None):
-        super().__init__(parent)
-        self.ip = ip
-        self.port = port
-        self.username = username
-        self.password = password
-        self.config_module = config_module
-
-    def run(self):
-        try:
-            with device_link.DeviceLink(self.ip, self.port, self.username, self.password) as link:
-                global_ms.text_print.emit(self.parent().ui.console_log, link.executive_command("enable\n"))
-                global_ms.text_print.emit(self.parent().ui.console_log, link.executive_command("configure terminal\n"))
-                for config in self.config_module:
-                    global_ms.text_print.emit(self.parent().ui.console_log, link.executive_command(config))
-                global_ms.text_print.emit(self.parent().ui.console_log, link.executive_command("\n"))
-                global_ms.text_print.emit(self.parent().ui.console_log, link.executive_command("end\n"))
-                global_ms.text_print.emit(self.parent().ui.console_log, link.executive_command("save config\n"))
-        except ConnectionError as e:
-            global_ms.text_print.emit(self.parent().ui.console_log, f"连接错误: {e}")
-        except Exception as e:
-            global_ms.text_print.emit(self.parent().ui.console_log, f"其他错误: {e}")
 
 
 class MainUI(QWidget):
@@ -58,8 +23,8 @@ class MainUI(QWidget):
         self.ui.module_select.addItems(["address", "service", "policy", "interface", "route"])
         self.ui.submit_button.clicked.connect(partial(self.submit_config))
         self.ui.clear_button.clicked.connect(partial(self.clear_text_button))
-        self.ui.port.setValidator(QtGui.QIntValidator())
-        global_ms.text_print.connect(self.update_console_log)
+        self.ui.port.setValidator(QIntValidator())
+        console_signals.text_print.connect(self.update_console_log)
 
     @staticmethod
     def update_console_log(browser: QTextBrowser, text: str):
@@ -87,7 +52,7 @@ class MainUI(QWidget):
         config = configuration_clear.Configuration(config_path)
         config_module = config.search_module(module_name)
 
-        thread = WorkerThread(ip, port, username, password, config_module, self)
+        thread = ExecutiveCommandThread(ip, port, username, password, config_module, self)
         thread.start()
 
     def clear_text_button(self):
@@ -100,4 +65,4 @@ if __name__ == '__main__':
     app.setWindowIcon(QIcon('../images/logo.png'))
     stats = MainUI()
     stats.ui.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
